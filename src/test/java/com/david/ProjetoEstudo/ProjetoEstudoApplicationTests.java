1cf821c188e1c68266173af8676224d9776aeb43
davidbs09 <davidbissaco6@hotmail.com>
2023-02-23 21:45:13 -0300

    commit inicial

---
+++ src/test/java/com/david/ProjetoEstudo/ProjetoEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.ProjetoEstudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
